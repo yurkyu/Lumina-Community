b5866e595767ebce83f6ad0cb7f159c01f89aa17
sute <suteofficial@gmail.com>
2024-04-10 15:34:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using CSInputs;
+using CSInputs.Enums;
+using Lumina_Community;
+using SharpHook;
+using SharpHook.Native;
+
+namespace Community
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("exmp..."); // This will write something on the console
+
+            Run();
+        }
+
+        private static void Run()  // Write your macro here! (Inside the brackets)
+        { // Do not delete Util.Focus();
+            Util.Focus();
+
+            // For example
+            CSInputs.SendInput.Keyboard.Send(KeyboardKeys.Escape, KeyFlags.Down); // Presses Escape
+
+        }
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lumina_Community
+{
+    internal class Util // Do not touch this file if you dont know what you are doing!
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public static void Focus()
+        {
+            try
+            {
+                Process[] processes = Process.GetProcessesByName("RobloxPlayerBeta");
+                SetForegroundWindow(processes[0].MainWindowHandle);
+            }
+            catch
+            {
+                Console.WriteLine("Roblox is not running");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
